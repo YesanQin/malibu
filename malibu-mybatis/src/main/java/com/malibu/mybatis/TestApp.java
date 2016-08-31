e7a571b7d8891bfa64190778f1f93e3e18ba2ab3
caoye <caoye@maihaoche.com>
2016-08-31 20:26:09 +0800

    添加置文件

---
+++ malibu-mybatis/src/main/java/com/malibu/mybatis/TestApp.java
@@ -0,0 +1,38 @@
+package com.malibu.mybatis;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Created by qinyisheng on 16/8/31.
+ */
+public class TestApp {
+
+    private static SqlSessionFactoryBuilder sqlSessionFactoryBuilder;
+    private static SqlSessionFactory sqlSessionFactory;
+
+    public static void main(String[] args) {
+        init();
+
+    }
+
+    public static void init(){
+        sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+        String resource = "mybatis-config.xml";
+        Reader reader = null;
+        try {
+           reader = Resources.getResourceAsReader(resource);
+        } catch (IOException e) {
+            System.out.println("获取配置文件失败!");
+            e.printStackTrace();
+        }
+        sqlSessionFactory = sqlSessionFactoryBuilder.build(reader);
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+
+    }
+}
