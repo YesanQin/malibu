bc790cc618e2dadf9369f5c11357cf59f01e66cd
caoye <caoye@maihaoche.com>
2016-09-02 17:03:56 +0800

    优化

--- malibu-web/src/main/java/com/malibu/web/controller/TestController.java
+++ malibu-web/src/main/java/com/malibu/web/controller/TestController.java
@@ -1,16 +1,20 @@
 package com.malibu.web.controller;
 
+import com.malibu.web.service.TestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by qinyisheng on 16/9/2.
  */
-@RestController
+@Controller
 public class TestController {
+    @Autowired
+    private TestService testService;
     @RequestMapping(value = "/test.html")
     public String showIndex(){
-        System.out.println("Hello MALIBU~");
+        testService.testMethod();
         return "";
     }
 }
---
+++ malibu-web/src/main/java/com/malibu/web/service/TestService.java
@@ -0,0 +1,15 @@
+package com.malibu.web.service;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by qinyisheng on 16/9/2.
+ */
+@Service
+public class TestService {
+
+    public void testMethod(){
+        System.out.println("Hello Malibu! @caoye");
+    }
+
+}
