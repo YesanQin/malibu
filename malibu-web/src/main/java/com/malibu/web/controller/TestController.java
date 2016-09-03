613c4d5bf59bce4cd92a2e67cdf8bb2256a2d192
caoye <caoye@maihaoche.com>
2016-09-03 16:34:14 +0800

    添加日志打印功Log4jConfigListene能

--- malibu-web/src/main/java/com/malibu/web/controller/TestController.java
+++ malibu-web/src/main/java/com/malibu/web/controller/TestController.java
@@ -1,6 +1,8 @@
 package com.malibu.web.controller;
 
 import com.malibu.web.service.TestService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,11 +12,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class TestController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestController.class);
     @Autowired
     private TestService testService;
 
     @RequestMapping(value = "/test.html")
     public String showIndex(){
+        LOGGER.info("info test");
+        LOGGER.debug("debug test");
+        LOGGER.error("Erroe test");
         testService.testMethod();
         return "test";
     }
--- malibu-web/src/main/webapp/index.jsp
+++ malibu-web/src/main/webapp/index.jsp
@@ -10,11 +10,13 @@
     <li>
         <p>视图</p>
         <p>freemarker</p>
+        <a href="http://blog.csdn.net/fhx007/article/details/7902040/" target="_blank">freemarker介绍文档</a>
     </li>
     <li>
         <p>日志</p>
         <p>log4j</p>
         <a href="http://www.codeceo.com/article/8-java-log-framework.html" target="_blank">java日志介绍</a>
+        <a href="http://blog.csdn.net/imxiangzi/article/details/7835860" target="_blank">log4j介绍</a>
     </li>
 </ul>
 </body>
