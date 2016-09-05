74fb80e47c2b6eeadc06de34e390204df8984851
caoye <caoye@maihaoche.com>
2016-09-05 11:48:26 +0800

    添加mongodb对外接口及其实现类

---
+++ malibu-mongodb/src/main/java/com/malibu/mongodb/impl/MongodbRepository.java
@@ -0,0 +1,9 @@
+package com.malibu.mongodb.impl;
+
+import com.malibu.mongodb.IMongodbRepository;
+
+/**
+ * Created by qinyisheng on 16/9/5.
+ */
+public class MongodbRepository implements IMongodbRepository {
+}
