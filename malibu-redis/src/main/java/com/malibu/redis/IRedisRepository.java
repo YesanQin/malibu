ac97b8b0f5f62e61b00bf65be7e53efeb84d4e96
caoye <caoye@maihaoche.com>
2016-09-05 11:46:19 +0800

    添加redis对外接口及其实现类

---
+++ malibu-redis/src/main/java/com/malibu/redis/IRedisRepository.java
@@ -0,0 +1,7 @@
+package com.malibu.redis;
+
+/**
+ * Created by qinyisheng on 16/9/5.
+ */
+public interface IRedisRepository {
+}
---
+++ malibu-redis/src/main/java/com/malibu/redis/impl/RedisRepository.java
@@ -0,0 +1,9 @@
+package com.malibu.redis.impl;
+
+import com.malibu.redis.IRedisRepository;
+
+/**
+ * Created by qinyisheng on 16/9/5.
+ */
+public class RedisRepository implements IRedisRepository {
+}
